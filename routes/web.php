21e27cefba00260d33b5429204b0a621a4230034
TOGNISSE BABATUNDE GILLES-CHRIST EXAUCE <gilchristtognisse@gmail.com>
2026-09-11 12:36:34 +0200

    Update csrf-cookie route definition

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
+<?php
+
 use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
 
 Route::get('/', function () {
     return response()->json(['message' => 'SIGS Admin API - voir /api/ping']);
 });
 
-// Force la route csrf-cookie si le package ne l'enregistre pas automatiquement en production
-Route::get('/sanctum/csrf-cookie', \Laravel\Sanctum\Http\Controllers\CsrfCookieController::class . '@show');
+// Force la route csrf-cookie pour éviter le 404 en production
+Route::get('/sanctum/csrf-cookie', [CsrfCookieController::class, 'show']);
