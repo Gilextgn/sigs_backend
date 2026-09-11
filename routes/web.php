6bbb8afee6ec5a9ca13adb0e250e3f6b9e430462
TOGNISSE BABATUNDE GILLES-CHRIST EXAUCE <gilchristtognisse@gmail.com>
2026-09-11 12:34:50 +0200

    Refactor route to use function instead of arrow function

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
-<?php
-
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => response()->json([
-    'message' => 'SIGS Admin API - voir /api/ping',
-]));
+Route::get('/', function () {
+    return response()->json(['message' => 'SIGS Admin API - voir /api/ping']);
+});
+
+// Force la route csrf-cookie si le package ne l'enregistre pas automatiquement en production
+Route::get('/sanctum/csrf-cookie', \Laravel\Sanctum\Http\Controllers\CsrfCookieController::class . '@show');
